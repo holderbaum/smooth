ea737e20c91083a85c69e05f2e80fda48efea4a7
Jakob Holderbaum <jh@neopoly.de>
2011-03-19 19:08:12 +0100

    Add api.example file, fileld with meta information.

---
+++ api-example.rb
@@ -0,0 +1,15 @@
+Smooth.new do
+
+  meta.title      "API"
+
+  meta.generator  "Organic"
+  meta.author     "J Holderbaum"
+  meta.company    "Foo"
+  meta.email      "mail@foo.bar"
+  meta.date       DateTime.now
+  meta.venue      "Interwebz"
+
+  slides do
+
+  end
+end
