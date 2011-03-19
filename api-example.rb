68b452565edca75ec5d5ffe8641fae71c794b0a4
Jakob Holderbaum <jh@neopoly.de>
2011-03-19 19:19:15 +0100

    Add slides api to the example file.

--- api-example.rb
+++ api-example.rb
@@ -11,5 +11,45 @@ Smooth.new do
 
   slides do
 
+    slide "Title of Slide #1" do
+      box "Some text in this section" do
+        p "Look at this, I'm in a paragraph"
+      end
+      
+      box "Look at this list" do
+        list do
+          * "Something"
+          * "In a list"
+          * "I hope, the star can be used as method name"
+          * "But since we are driving ruby, I don't see any problems"
+          * "With this ^^"
+        end
+      end
+    end
+
+    slide "Vertical centering" do
+      box "This content is vertical centered", :vcenter => true do
+        p "This is the box content"
+      end
+    end
+
+    slide "Syntax highlighting" do
+      box "Syntax highlighting is very simple:" do
+        code :ruby, <<-EOC
+          class Syntax
+            def case_sensitive
+              true
+            end
+          end
+        EOC
+      end
+    end
+
+    slide "Boxes" do
+      box do
+        p "A box can be used without a title, too."
+      end
+    end
+
   end
 end
