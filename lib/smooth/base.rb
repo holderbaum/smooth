f6cbfff961f6228e52699ac3bf5c24c042521044
Jakob Holderbaum <jh@neopoly.de>
2011-03-21 23:11:44 +0100

    Implement placeholder through a content_store.

--- lib/smooth/base.rb
+++ lib/smooth/base.rb
@@ -1,12 +1,14 @@
 module Smooth
   class Base
+    include Smooth::Helpers
 
     def initialize(layout)
       @layout = layout  
     end
 
-    def render(template)
-
+    def render(content)
+      content_store = ContentStore.new(content)
+      Haml::Engine.new(@layout).render(content_store)
     end
 
   end
