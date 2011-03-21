2df355945e000084251c5bf338cb093cd2b98fc7
Jakob Holderbaum <jh@neopoly.de>
2011-03-21 22:28:30 +0100

    Add basic layout test.

--- test/base_test.rb
+++ test/base_test.rb
@@ -1,9 +1,29 @@
 require 'test/helper'
+require 'lib/smooth/base'
 
 class SmoothBaseTest < Test::Unit::TestCase
+  include Smooth
 
-  test "something" do
-    assert false
+  context "Layout" do
+    test "it should render everything in the layout" do
+      layout = <<-EOC
+.html
+  .body
+    =slides
+      EOC
+      result = <<-EOC
+<html>
+  <body>
+    <div class="slides">
+      <p>stuff</p>
+    </div>
+  </body>
+</html>
+      EOC
+
+      smooth = Base.new(layout)
+      assert_equal result, smooth.render(".p stuff")
+    end
   end
 
 end
