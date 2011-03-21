34141cccfe39f661316302217a493c4f280ba57e
Jakob Holderbaum <jh@neopoly.de>
2011-03-21 23:41:41 +0100

    Implement register_helper and use an embedded RenderContext-Class.

--- lib/smooth/helpers/content_store.rb
+++ lib/smooth/helpers/content_store.rb
@@ -2,19 +2,35 @@ module Smooth
   module Helpers
     class ContentStore
 
+      class RenderContext
+        def initialize
+          @content = {}
+        end
+
+        def content(title, &block)
+          @content[title] = capture_haml(&block) if block_given?
+          @content[title]
+        end
+      end
+
+      def self.register_helpers(mod)
+        RenderContext.send :include, mod
+      end
+
       def initialize(layout)
         @content = {}
+        @context = RenderContext.new
         @layout = layout 
       end
       
       def render!
-        Haml::Engine.new(@layout).render(self)
+        Haml::Engine.new(@layout).render(@context)
       end
 
-      def content(title, &block)
-        @content[title] = capture_haml(&block) if block_given?
-        @content[title]
+      def content(title)
+        @context.content(title)
       end
+
     end
   end
 end
