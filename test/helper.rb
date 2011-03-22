b6e26ef21de97cb10f3bd02a161f37111340a018
Jakob Holderbaum <jh@neopoly.de>
2011-03-22 22:14:16 +0100

    Add a unindent method to string.

--- test/helper.rb
+++ test/helper.rb
@@ -4,6 +4,13 @@ $LOAD_PATH.unshift File.dirname(__FILE__)
 
 require 'contest'
 
+class String
+  def unindent
+    indent = self[/^\s+/]
+    self.gsub(/^#{indent}/, '')
+  end
+end
+
 class Test::Unit::TestCase
   # something
 end
