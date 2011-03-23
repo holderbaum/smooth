9b6e13bd75c07999a1a732fc285aba00f59136b9
Jakob Holderbaum <jh@neopoly.de>
2011-03-23 21:01:44 +0100

    Implement js_include.

--- lib/smooth/helpers/assets.rb
+++ lib/smooth/helpers/assets.rb
@@ -3,6 +3,16 @@ module Smooth
     module Assets
       BASE_PATH = File.expand_path('../../../../assets', __FILE__)
       JS_PATH = File.join(BASE_PATH, 'js')
+
+      def js_include(*args)
+        path = ['js'].concat(args).map(&:to_s)
+
+        path.last << '.js'
+        path = File.join(path)
+
+        haml_tag 'script', :src => path
+      end
+
     end
   end
 end
