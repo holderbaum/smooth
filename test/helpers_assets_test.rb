c3d6a5fa9a8fe493bd987a2a2d554c4c809f9b45
Jakob Holderbaum <jh@neopoly.de>
2011-03-23 19:55:42 +0100

    Test first js, not css and add a in_test_dir do helper.

--- test/helpers_assets_test.rb
+++ test/helpers_assets_test.rb
@@ -11,31 +11,39 @@ class SmoothHelpersComponentsTest < Test::Unit::TestCase
     cs
   end
 
+  def in_test_dir(&block)
+    path = File.expand_path('../fixtures/test_dir', __FILE__)
+    Dir.chdir(path, &block)
+  end
+
   context "no setup" do
     test "it should set the correct path" do
       path = File.expand_path('../../assets', __FILE__)
       assert_equal path, Smooth::Helpers::Assets::BASE_PATH
-      assert_equal File.join(path, 'css'), Smooth::Helpers::Assets::CSS_PATH
+      assert_equal File.join(path, 'js'), Smooth::Helpers::Assets::JS_PATH
     end
   end
 
   context "PATH set by setup" do
     setup do
       @old_base_path = Smooth::Helpers::Assets::BASE_PATH
-      @old_css_path  = Smooth::Helpers::Assets::CSS_PATH
+      @old_js_path  = Smooth::Helpers::Assets::JS_PATH
       Smooth::Helpers::Assets::BASE_PATH = File.expand_path("../fixtures/assets", __FILE__)
-      Smooth::Helpers::Assets::CSS_PATH = File.join(Smooth::Helpers::Assets::BASE_PATH, 'css')
+      Smooth::Helpers::Assets::JS_PATH = File.join(Smooth::Helpers::Assets::BASE_PATH, 'js')
     end
 
     teardown do
       Smooth::Helpers::Assets::BASE_PATH = @old_base_path
-      Smooth::Helpers::Assets::CSS_PATH = @old_css_path
+      Smooth::Helpers::Assets::JS_PATH = @old_js_path
     end
 
-    context "css" do
+    context "js" do
       
-      test "" do
-
+      test "it should copy the js file and include it" do
+        in_test_dir do
+          puts Dir.pwd
+        end
+        puts Dir.pwd
       end
 
     end
