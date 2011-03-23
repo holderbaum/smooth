5e685315c56883e22fce7df99ced88a818016dc3
Jakob Holderbaum <jh@neopoly.de>
2011-03-23 19:44:13 +0100

    Add empty assets helper module-

---
+++ lib/smooth/helpers/assets.rb
@@ -0,0 +1,8 @@
+module Smooth
+  module Helpers
+    module Assets
+      BASE_PATH = File.expand_path('../../../../assets', __FILE__)
+      CSS_PATH = File.join(BASE_PATH, 'css')
+    end
+  end
+end
