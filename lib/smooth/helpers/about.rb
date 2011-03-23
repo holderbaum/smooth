422cd89d7e5eccca0a13a9e0ba727e766bac7b8c
Jakob Holderbaum <jh@neopoly.de>
2011-03-23 19:13:07 +0100

    Add a context class for block yielding to about.

--- lib/smooth/helpers/about.rb
+++ lib/smooth/helpers/about.rb
@@ -1,6 +1,29 @@
 module Smooth
   module Helpers
     module About
+      class Context
+
+        def initialize(parent)
+          @parent = parent
+        end
+
+        def eval(&block)
+          instance_eval(&block)
+        end
+
+        def add_about_info(key, value)
+          @parent.about[key] = value
+        end
+
+        def method_missing(meth, *args, &block)
+          if args.size > 0
+            add_about_info meth, args.first
+          end
+        end
+      end
+
+      attr_reader :about
+
       def about(title = nil, &block)
         @about ||= {}
 
@@ -8,34 +31,13 @@ module Smooth
           return @about[title]
         end
 
-        content(:about, &block) if block_given?
+        if block_given?
+          Context.new(self).eval(&block)
+        end
+
         @about
       end
 
-      # TODO: method missing is our friend here
-      def title(s)
-        @about[:title] = s
-      end
-
-      def author(s)
-        @about[:author] = s
-      end
-
-      def company(s)
-        @about[:company] = s
-      end
-
-      def email(s)
-        @about[:email] = s
-      end
-
-      def date(s)
-        @about[:date] = s
-      end
-
-      def venue(s)
-        @about[:venue] = s
-      end
     end
   end
 end
