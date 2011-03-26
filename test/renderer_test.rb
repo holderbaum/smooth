77b4298515fc3a66be776122032b7c50a6fe7d26
Jakob Holderbaum <jh@neopoly.de>
2011-03-26 14:15:48 +0100

    Add a test for the shared scope between template and layout.

--- test/renderer_test.rb
+++ test/renderer_test.rb
@@ -24,4 +24,29 @@ class SmoothRendererTest < Test::Unit::TestCase
 
   end
 
+  context "rendering with layout" do
+    test "it should give the template context after rendering to the layout renderer" do
+      template = <<-EOC.unindent
+        .around
+          - @inner = 42
+      EOC
+
+      layout = <<-EOC.unindent
+        .layout
+          = @inner
+      EOC
+
+      html = <<-EOC.unindent
+        <div class='layout'>
+          42
+        </div>
+      EOC
+
+      r = Smooth::Renderer.new(template)
+      r.layout = layout
+
+      assert_equal html, r.result
+    end
+  end
+
 end
