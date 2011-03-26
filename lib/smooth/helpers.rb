3f4fbf2948799ff3a77ac0696bcb3f8b90310a1b
Jakob Holderbaum <jh@neopoly.de>
2011-03-26 15:09:45 +0100

    Remove helper registerings at ContentStore from helpers.rb

--- lib/smooth/helpers.rb
+++ lib/smooth/helpers.rb
@@ -5,16 +5,9 @@ module Smooth
   module Helpers
 
     require 'smooth/helpers/about'
-    ContentStore.register_helpers About
-
     require 'smooth/helpers/slides'
-    ContentStore.register_helpers Slides
-
     require 'smooth/helpers/components'
-    ContentStore.register_helpers Components
-
     require 'smooth/helpers/assets'
-    ContentStore.register_helpers Assets
 
   end
 end
