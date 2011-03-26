ae9fe2e09e3207f436588f86ff443dda6b67319c
Jakob Holderbaum <jh@neopoly.de>
2011-03-26 14:28:50 +0100

    Add test for the instance-wide inclusiono of helpers into the renderer.

--- test/renderer_test.rb
+++ test/renderer_test.rb
@@ -49,4 +49,34 @@ class SmoothRendererTest < Test::Unit::TestCase
     end
   end
 
+  context "registering helpers to the context" do
+    test "it should only be instance-wide" do
+      template = <<-EOC.unindent
+        .around
+          =my_helper
+      EOC
+
+      html = <<-EOC.unindent
+        <div class='around'>
+          I'm in ur helperz
+        </div>
+      EOC
+
+      module MyTestHelpers
+        def my_helper
+          "I'm in ur helperz"
+        end
+      end
+
+      r = Smooth::Renderer.new(template)
+      r.register_helpers MyTestHelpers
+      assert_equal html, r.result
+
+      assert_raise NameError do
+        r = Smooth::Renderer.new(template)
+        r.result
+      end
+    end
+  end
+
 end
