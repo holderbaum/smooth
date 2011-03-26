73a7ef0e4242354e3215f5d877e1e561b62807ed
Jakob Holderbaum <jh@neopoly.de>
2011-03-26 15:10:11 +0100

    Make ContentStore to a module with just the context functionality.

--- lib/smooth/helpers/content_store.rb
+++ lib/smooth/helpers/content_store.rb
@@ -1,45 +1,17 @@
 module Smooth
   module Helpers
-    class ContentStore
+    module ContentStore
 
-      class RenderContext
-        def initialize
-          @content = {}
+      def content(title, string=nil, &block)
+        @content ||= {}
+
+        if string
+          @content[title] = "#{string}\n"
+        elsif block_given?
+          @content[title] = capture_haml(&block) if block_given?
+        else
+          @content[title]
         end
-
-        def content(title, string=nil, &block)
-          if string
-            @content[title] = "#{string}\n"
-          elsif block_given?
-            @content[title] = capture_haml(&block) if block_given?
-          else
-            @content[title]
-          end
-        end
-
-        def method_missing(meth, *args, &block)
-          component_resolver( *([meth, args].flatten), &block )
-        end
-      end
-
-      def self.register_helpers(mod)
-        RenderContext.send :include, mod
-      end
-
-      attr_reader :context
-
-      def initialize(template)
-        @content = {}
-        @context = RenderContext.new
-        @template = template 
-      end
-      
-      def render!
-        Haml::Engine.new(@template).render(@context)
-      end
-
-      def content(title)
-        @context.content(title)
       end
 
     end
--- test/helpers_content_store_test.rb
+++ test/helpers_content_store_test.rb
@@ -4,8 +4,14 @@ require 'lib/smooth'
 class SmoothHelpersContentStoreTest < Test::Unit::TestCase
   include Smooth::Helpers
 
+  def renderer_context(haml)
+    r = renderer(haml, nil, Smooth::Helpers::ContentStore)
+    r.result
+    r.context
+  end
+
   context "rendering" do
-    test "it should render a haml file into content_stores" do
+    test "it should render a haml file into the content hash" do
       haml = <<-EOC.unindent
         -content :about do
           .something
@@ -13,72 +19,22 @@ class SmoothHelpersContentStoreTest < Test::Unit::TestCase
         -content :slides do
           .other
       EOC
-      cs = ContentStore.new(haml)
-      cs.render!
 
-      assert_equal "<div class='something'></div>\n", cs.context.content(:about)
-      assert_equal "<div class='other'></div>\n", cs.context.content(:slides)
+      r = renderer_context(haml)
+
+      assert_equal "<div class='something'></div>\n", r.content(:about)
+      assert_equal "<div class='other'></div>\n", r.content(:slides)
     end
 
     test "content should also accept a html string" do
       haml = <<-EOC.unindent
         -content :string, "the content"
       EOC
-      cs = ContentStore.new(haml)
-      cs.render!
 
-      assert_equal "the content\n", cs.context.content(:string)
+      r = renderer_context(haml)
+
+      assert_equal "the content\n", r.content(:string)
     end
   end
 
-  context "plugins" do
-    test "it should be extendable with helpers" do
-      module ExampleHelperModule
-        def weird_method_name
-          "return"
-        end
-      end
-
-      ContentStore.register_helpers(ExampleHelperModule)
-      cs = ContentStore.new("")
-
-      assert_equal "return", cs.context.weird_method_name
-    end
-  end
-
-  context "method missing" do
-    setup do
-      new_pathes = []
-      new_pathes << File.expand_path("../fixtures/components/path1", __FILE__)
-      new_pathes << File.expand_path("../fixtures/components/path2", __FILE__)
-
-      Smooth::Helpers::Components.clear_pathes!
-      Smooth::Helpers::Components.unshift_pathes new_pathes[0]
-      Smooth::Helpers::Components.unshift_pathes new_pathes[1]
-    end
-
-    teardown do
-      Smooth::Helpers::Components.reset_pathes!
-    end
-
-    test "it should wrap component_resolver with method_missing" do
-      haml = <<-EOC.unindent
-        -content :test1 do
-          =missing "the title" do
-            .stuff
-      EOC
-
-      result = <<-EOC.unindent
-        <div class='missing'>
-          the title
-          <div class='stuff'></div>
-        </div>
-      EOC
-
-      ContentStore.register_helpers( Smooth::Helpers::Components )
-      cs = ContentStore.new(haml)
-      cs.render!
-      assert_equal result, cs.context.content(:test1)
-    end
-  end
 end
