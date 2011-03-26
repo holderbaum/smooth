3537f158e932f4ddb9cf2201ab505eac11788c61
Jakob Holderbaum <jh@neopoly.de>
2011-03-26 14:15:57 +0100

    Implement the shared scope.

--- lib/smooth/renderer.rb
+++ lib/smooth/renderer.rb
@@ -1,17 +1,21 @@
 module Smooth
   class Renderer
 
+    attr_writer :layout
+
     def initialize(template)
       @template = template
     end
 
     def render!
-      Haml::Engine.new(@template).render
+      context = Object.new
+      result = Haml::Engine.new(@template).render(context)
+      result = Haml::Engine.new(@layout).render(context) if @layout
+      result
     end
 
     def result
       @result ||= render!
     end
-
   end
 end
