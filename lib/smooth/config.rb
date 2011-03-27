47d628eb02069fd816d3e59d91a1910406ae904d
Jakob Holderbaum <jh@neopoly.de>
2011-03-27 13:09:27 +0200

    Add tests for components_pathes and asserts_path.

--- lib/smooth/config.rb
+++ lib/smooth/config.rb
@@ -1,5 +1,33 @@
 module Smooth
   class Config
 
+    ASSETS_PATH = File.expand_path('../../../assets', __FILE__)
+    COMPONENTS_PATH = File.expand_path("../../../comp", __FILE__)
+
+    attr_reader :assets_path, :components_pathes
+
+    def initialize
+      @assets_path = Pathname.new( ASSETS_PATH ) 
+      @components_pathes = Pathes.new( COMPONENTS_PATH )
+    end
+
+
+    class Pathes
+      def initialize(*args)
+        @pathes = []
+        args.each do |path|
+          @pathes << path
+        end
+      end
+
+      def to_a
+        @pathes.dup
+      end
+
+      def each(&blk)
+        @pathes.each(&blk)
+      end
+    end
+
   end
 end
--- test/config_test.rb
+++ test/config_test.rb
@@ -5,7 +5,16 @@ class SmoothConfigTest < Test::Unit::TestCase
 
   context "initialize" do
 
-    cfg = Smooth::Config.new
+    test "default pathes" do
+      cfg = Smooth::Config.new
+
+      assets_path = Pathname.new(File.expand_path('../../assets', __FILE__))
+      components_pathes = Smooth::Config::Pathes.new(File.expand_path('../../comp', __FILE__))
+
+      assert_equal assets_path, cfg.assets_path
+      assert cfg.components_pathes.is_a?(Smooth::Config::Pathes)
+      assert_equal components_pathes.to_a, cfg.components_pathes.to_a
+    end
 
   end
 
