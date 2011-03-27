ccfb7881b88f1bbf48ca596e3868e82d62fa9af2
Jakob Holderbaum <jh@neopoly.de>
2011-03-27 20:43:22 +0200

    Add first test and empty class for the style helper.

---
+++ lib/smooth/helpers/style.rb
@@ -0,0 +1,15 @@
+module Smooth
+  module Helpers
+    module Style
+
+      #def slides(&block)
+        #if block_given?
+          #content :slides, component(:slides, {}, &block)
+        #else
+          #content :slides
+        #end
+      #end
+
+    end
+  end
+end
---
+++ test/helpers_style_test.rb
@@ -0,0 +1,31 @@
+require 'test/helper'
+require 'lib/smooth'
+
+class SmoothHelpersStyleTest < Test::Unit::TestCase
+
+  def renderer_context(haml)
+    r = renderer(haml, nil, Smooth::Config.new, [Smooth::Helpers::ContentStore, Smooth::Helpers::Style])
+    r.result
+    r.context
+  end
+
+
+  test "it should provide a collecting style helper" do
+    haml = <<-EOC.unindent
+      -style :style1 do
+        my css
+      -style :style2 do
+        my css2
+    EOC
+
+    result = <<-EOC.unindent
+      // style1
+      my css
+
+      // style2
+      my css2
+    EOC
+
+    assert_equal result, renderer_context(haml).styles
+  end
+end
