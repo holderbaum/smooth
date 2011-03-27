94dc26fa805abae81fcaf94d72222e0b188e4ceb
Jakob Holderbaum <jh@neopoly.de>
2011-03-27 22:29:54 +0200

    Require ultraviolet in smooth.rb.

--- lib/smooth.rb
+++ lib/smooth.rb
@@ -1,8 +1,11 @@
-require 'rubygems'
-require 'haml'
 require 'pathname'
 require 'fileutils'
 require 'forwardable'
+
+require 'rubygems'
+require 'haml'
+require 'uv'
+
 
 require 'smooth/config'
 
