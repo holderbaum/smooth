8bbf3bfbf91e21aba29a22c71aff56c49cf1e559
Jakob Holderbaum <jh@neopoly.de>
2011-03-27 16:39:55 +0200

    Add tests for the file-access of layout and implement it based on config.layouts_path.

--- lib/smooth/helpers/layout.rb
+++ lib/smooth/helpers/layout.rb
@@ -2,6 +2,18 @@ module Smooth
   module Helpers
     module Layout
 
+      def layout(name)
+        name = name.to_s
+
+        name << ".haml" unless name.match(/\.haml$/)
+          
+        @config.layouts_pathes.each do |path|
+          if(file = path.join(name)).exist?
+            set_layout_haml_content( file.read )
+          end
+        end
+      end
+
       def set_layout_haml_content(layout)
         @renderer.layout = layout
       end
--- test/helpers_layout_test.rb
+++ test/helpers_layout_test.rb
@@ -4,14 +4,15 @@ require 'lib/smooth'
 class SmoothHelpersLayoutTest < Test::Unit::TestCase
 
   CONFIG = Smooth::Config.new
-  CONFIG.layouts_pathes.prepend File.expand_path("../fixtures/layouts", __FILE__)
+  CONFIG.layouts_pathes.prepend File.expand_path("../fixtures/layouts/path1", __FILE__)
+  CONFIG.layouts_pathes.prepend File.expand_path("../fixtures/layouts/path2", __FILE__)
 
   def renderer(haml)
     r = super(haml, nil, CONFIG, Smooth::Helpers::Layout)
     r.result
   end
 
-  test "it shoul be possible to set raw haml content as layout" do
+  test "it should be possible to set raw haml content as layout" do
     template = <<-EOC.unindent
       -set_layout_haml_content ".layout= @inner"
       -@inner = "foo"
@@ -22,5 +23,71 @@ class SmoothHelpersLayoutTest < Test::Unit::TestCase
     EOC
 
     assert_equal result, renderer(template)
+  end
+
+  test "it should use the layout from layouts pathes" do
+    templates = []
+    templates << <<-EOC.unindent
+      -layout :test1
+      -@inner = "foo"
+    EOC
+
+    templates << <<-EOC.unindent
+      -layout :test2
+      -@inner = "foo"
+    EOC
+
+    results = []
+    results << <<-EOC.unindent
+      <div class='test-1'>
+        <div class='layout'>
+          foo
+        </div>
+      </div>
+    EOC
+
+    results << <<-EOC.unindent
+      <div class='test-2'>
+        <div class='layout'>
+          foo
+        </div>
+      </div>
+    EOC
+
+    assert_equal results[0], renderer(templates[0])
+    assert_equal results[1], renderer(templates[1])
+  end
+
+  test "it should accept strings with and without .haml" do
+    templates = []
+    templates << <<-EOC.unindent
+      -layout "test1"
+      -@inner = "foo"
+    EOC
+
+    templates << <<-EOC.unindent
+      -layout "test2.haml"
+      -@inner = "foo"
+    EOC
+
+    results = []
+    results << <<-EOC.unindent
+      <div class='test-1'>
+        <div class='layout'>
+          foo
+        </div>
+      </div>
+    EOC
+
+    results << <<-EOC.unindent
+      <div class='test-2'>
+        <div class='layout'>
+          foo
+        </div>
+      </div>
+    EOC
+
+    assert_equal results[0], renderer(templates[0])
+    assert_equal results[1], renderer(templates[1])
   end
 end
