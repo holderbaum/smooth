01e56f23020841a670f095294bad97caffbff852
Jakob Holderbaum <jh@neopoly.de>
2011-03-27 12:53:58 +0200

    Add all external requires to smooth.rb.

--- lib/smooth.rb
+++ lib/smooth.rb
@@ -1,5 +1,9 @@
 require 'rubygems'
 require 'haml'
+require 'pathname'
+require 'fileutils'
+
+require 'smooth/config'
 
 require 'smooth/helpers'
 require 'smooth/renderer'
--- lib/smooth/helpers.rb
+++ lib/smooth/helpers.rb
@@ -1,6 +1,4 @@
 require 'smooth/helpers/content_store'
-require 'fileutils'
-
 require 'smooth/helpers/about'
 require 'smooth/helpers/slides'
 require 'smooth/helpers/components'
