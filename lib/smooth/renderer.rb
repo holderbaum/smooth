f13476ecfec301151996c923670043af2626e250
Jakob Holderbaum <jh@neopoly.de>
2011-03-29 17:40:42 +0200

    Add doc for renderer.

--- lib/smooth/renderer.rb
+++ lib/smooth/renderer.rb
@@ -1,7 +1,29 @@
 module Smooth
+  # The Renderer is the base of Smooth. 
+  # It renders the written template in a internally instanciated context. 
+  # This context can be extended by helper modules through @register_helpers@
+  #
+  # *Note:*
+  #
+  # Basically, the context equals self in your template.
+  #
+  # *Usage:*
+  #
+  # <pre>
+  # <code>
+  # smooth = Renderer.new("%p my haml template string")
+  #
+  # smooth.register_helpers Smooth::Helpers::About
+  # smooth.layout "= yield" # an optional layout (should be defined by the template)
+  #
+  # smooth.result #=> "<p>my haml template string</p>\n"
+  # </code>
+  # </pre>
   class Renderer
 
+    # Returns the layout haml string
     attr_accessor :layout
+    # Returns the context that has been used to render the template
     attr_reader :context
 
     def initialize(template, config = Config.new)
@@ -10,18 +32,21 @@ module Smooth
       @context = Context.new(self, config)
     end
 
-    def render!
-      result = Haml::Engine.new(@template).render(@context)
-      result = Haml::Engine.new(@layout).render(@context) if @layout
-      result
-    end
-
+    # Calls the render! method implicit (only once!) and returns the generated HTML 
     def result
       @result ||= render!
     end
 
+    # Patches all methods definde in the given module into the renderer context
     def register_helpers(mod)
       @context.register_helpers mod
+    end
+
+    private
+    def render!
+      result = Haml::Engine.new(@template).render(@context)
+      result = Haml::Engine.new(@layout).render(@context) if @layout
+      result
     end
 
     class Context
