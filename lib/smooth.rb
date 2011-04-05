50b0043b2a6554a012ade3eb16a778afa6d2ba68
Jakob Holderbaum <jh@neopoly.de>
2011-04-05 20:56:09 +0200

    Add thor and cli to smooth.rb

--- lib/smooth.rb
+++ lib/smooth.rb
@@ -5,9 +5,11 @@ require 'forwardable'
 require 'rubygems'
 require 'haml'
 require 'uv'
-
+require 'thor'
 
 require 'smooth/config'
 
 require 'smooth/helpers'
 require 'smooth/renderer'
+
+require 'smooth/cli'
