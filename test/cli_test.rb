9c39cba8482495d2203c20fd7a2d8fbc053e1c6c
Jakob Holderbaum <jh@neopoly.de>
2011-04-05 21:48:10 +0200

    Add test for generic CLI build call.

--- test/cli_test.rb
+++ test/cli_test.rb
@@ -3,5 +3,16 @@ require 'lib/smooth'
 
 class SmoothCLITest < Test::Unit::TestCase
 
+  context 'build' do
+    test "it should create the whole project" do
+      init_haml_template
+      in_test_dir do
+        Smooth::CLI.new.build
+
+        assert_slides_creation
+      end
+      clear_test_dir
+    end
+  end
 
 end
