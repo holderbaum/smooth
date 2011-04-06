d2c28bb83efb9c427c45fafabf55af0fa533f1a5
Jakob Holderbaum <jh@neopoly.de>
2011-04-06 07:29:33 +0200

    Add basic render capability to the build task.

--- lib/smooth/cli.rb
+++ lib/smooth/cli.rb
@@ -4,7 +4,18 @@ module Smooth
 
     desc :build, "create slides from template.haml"
     def build
-      puts "ohh"
+      template = Pathname.new File.expand_path('template.haml')
+      slides = Pathname.new File.expand_path('index.html')
+
+      renderer = Smooth::Renderer.new(template.read)
+
+      Smooth::Helpers.constants.each do |const|
+        renderer.register_helpers Smooth::Helpers.const_get(const)  
+      end
+
+      slides.open 'w' do |f|
+        f << renderer.result
+      end
     end
 
   end
